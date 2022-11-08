ce18bfd4a6c4c7dec4411519eba2d8937b6fcee2
mochimochit <plain_4362@yahoo.co.jp>
2022-11-08 17:39:17 +0900

    add show.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,15 @@ class PostsController < ApplicationController
   def postslist
     @posts = Post.all
   end
+
+  def show
+    @id = params[:id]
+    @post = Post.find_by(id:params[:id])
+  end
+
+  def create
+    @post = Post.new(content:params[:content])
+    @post.save
+    redirect_to("/postslist")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'login' => 'home#login'
   get 'profile' => 'home#profile'
+  get 'posts/:id' => 'posts#show'
+
+  post 'posts/create' => 'posts#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
