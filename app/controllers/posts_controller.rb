7be3c81049e9cc7d8db7ec56d30fd389037ac754
mochimochit <plain_4362@yahoo.co.jp>
2022-11-15 17:53:04 +0900

    add update destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def posts
-    @posts = Post.all
+    @posts = Post.new
   end
 
   def postslist
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -14,7 +14,31 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(content:params[:content])
-    @post.save
-    redirect_to("/postslist")
+    if @post.save
+      redirect_to("/postslist")
+    else
+      render("posts/post")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id:params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id:params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to('/postslist')
+    else
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id:params[:id])
+    @post.destroy
+    redirect_to('/postslist')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   get 'posts' => 'posts#post'
   get 'postslist' => 'posts#postslist'
+  get 'posts/:id/edit' => 'posts#edit'
   get '/' => 'home#top'
   get 'login' => 'home#login'
   get 'profile' => 'home#profile'
   get 'posts/:id' => 'posts#show'
 
   post 'posts/create' => 'posts#create'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
